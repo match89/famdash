1af6a75377671edb8871e4547c197015ddc8fdef
Matej Chudo <matej.chudo@siemens-healthineers.com>
2025-01-26 22:26:22 +0100

    set env variables during release

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Famdash.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
     ]
   end
 
